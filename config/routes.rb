788576b8d6afe29e4332807d147346788a3c6aa0
nanashimada <shimadanana@shimadanananoMacBook-puro.local>
2018-12-05 13:10:54 +0900

    modify "only" expression in routes
    onlyに：つけ忘れを修正。

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   root 'groups#index'
 
-  resource :groups, only[:new, :create, :edit, :update] do
-    resource :messages, only[:index, :create]
+  resource :groups, only:[:new, :create, :edit, :update] do
+    resource :messages, only:[:index, :create]
   end
 
   resources :users, only: [:index, :edit, :update]
