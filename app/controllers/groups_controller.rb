2cc5e2917c36758dee4387379506a8f70b8b530b
nanashimada <shimadanana@shimadanananoMacBook-puro.local>
2018-12-06 14:57:34 +0900

    Add group-edit-function
    グループの編集機能を実装。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
 
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
 
@@ -15,18 +17,27 @@ class GroupsController < ApplicationController
   	else
   		render :new
     end
-
   end
-
+  
   def edit
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
   	params.require(:group).permit(:name, {:user_ids => []})
+  end
+
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 
