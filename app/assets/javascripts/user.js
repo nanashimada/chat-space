1e77fb633ba633d0f2b7361a594bef2f3a43338f
nanashimada <shimadanana@shimadanananoMacBook-puro.local>
2018-12-13 18:56:00 +0900

    chage snake case to camelcase
    全てキャメルケースに統一しました。

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,20 +1,20 @@
 $(function(){
 
-	var user_list = $("#user-search-result");
-	var chat_member = $(".chat-group-member");
+	var userList = $("#user-search-result");
+	var chatMember = $(".chat-group-member");
 
 	function appendUser(user) {
 		var html = `<div class="chat-group-user clearfix" id="chat-member">
   						<p class="chat-group-user__name">${user.name}</p>
   						<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
 					</div>`
-		user_list.append(html);
+		userList.append(html);
 	}
 	function appendNoUser(user) {
 		var html = `<div class="chat-group-user clearfix">
   						${ user }
 					</div>`
-		user_list.append(html);
+		userList.append(html);
 	}
 
 	function appendGroupUser(user_id, user_name){
@@ -23,7 +23,7 @@ $(function(){
   						<p class="chat-group-user__name">${user_name}</p>
   						<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
 					</div>`
-		chat_member.append(html);
+		chatMember.append(html);
 	}
 
   	$("#user-search-field").on("keyup",function() {
@@ -37,7 +37,7 @@ $(function(){
     	})
 
    		.done(function(users) {
-   		user_list.empty();
+   		userList.empty();
      		if (users.length !==0) {
      			users.forEach(function(user){
       				appendUser(user);
@@ -53,9 +53,9 @@ $(function(){
 
     	$(document).on("click",".user-search-add",function(){
     		$(this).parent().remove();
-    		var user_id = $(this).attr("data-user-id");
-    		var user_name = $(this).attr("data-user-name");
-    			appendGroupUser(user_id, user_name);
+    		var userId = $(this).attr("data-user-id");
+    		var userName = $(this).attr("data-user-name");
+    			appendGroupUser(userId, userName);
     	})
     	$(document).on("click",".user-search-remove",function(){
     		$("#chat-group-user-8").remove();
